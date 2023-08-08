dd43e07798e766fb846404397739c4accfef3bbc
Jose Llanas <josellanasmota86@gmail.com>
2023-08-08 20:47:59 +0200

    MovieRepository - Implementación

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MovieRepository {
+
+  final MovieRepositoryImpl datasource;
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+}
