1684436b4a104cd663afb5c494ddd530014d796b
Jose Llanas <josellanasmota86@gmail.com>
2023-08-08 13:11:14 +0200

    datasources - get movies

---
+++ lib/infrastructure/moviedb_datasource.dart
@@ -0,0 +1,19 @@
+import 'package:cinemapedia/config/constants/environment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:dio/dio.dart';
+
+class MoviedbDatasource extends MoviesDatasource {
+  final dio = Dio(
+      BaseOptions(baseUrl: 'https://api.themoviedb.org/3', queryParameters: {
+    'api-key': Environment.theMovieDbKey,
+  }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+    final List<Movie> movies = [];
+
+    return movies;
+  }
+}
