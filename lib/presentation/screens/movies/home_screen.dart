67f604e8ab8743c2b9e635be0d082c5e357b7554
Jose Llanas <josellanasmota86@gmail.com>
2023-08-11 11:02:19 +0200

    custom appbar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -34,14 +35,25 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
     if (nowPlayingMovies.length == 0) return CircularProgressIndicator();
 
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        final movie = nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      },
+    return Column(
+      
+      children: [
+
+
+      CustomAppbar(),
+
+      Expanded(
+        child: ListView.builder(
+        itemCount: nowPlayingMovies.length,
+        itemBuilder: (context, index) {
+          final movie = nowPlayingMovies[index];
+          return ListTile(
+            title: Text(movie.title),
+            );
+          },
+        ),
+        )
+      ],
     );
   }
 }
