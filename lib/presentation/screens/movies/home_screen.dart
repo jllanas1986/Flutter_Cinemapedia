d03fa432058cb646dab3b6fcaaca279c1c1a817e
Jose Llanas <josellanasmota86@gmail.com>
2023-08-16 13:50:55 +0200

    fullScreen Loader design

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -44,6 +44,8 @@ class _HomeViewState extends ConsumerState<_HomeView> {
     final upcomingMovies = ref.watch(upcomingMoviesProvider);
     final topRatedMovies = ref.watch(topRatedMoviesProvider);
 
+    return const FullScreenLoader();
+
     return CustomScrollView(slivers: [
       const SliverAppBar(
         floating: true,
---
+++ lib/presentation/widgets/shared/full_screen_loader.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class FullScreenLoader extends StatelessWidget {
+  const FullScreenLoader({super.key});
+
+  Stream<String> getLoadingMessages() {
+    final messages = <String>[
+      'Cargando películas',
+      'Comprando palomitas de maiz',
+      'Cargando populares',
+      'Llamando a mi novia',
+      'Ya casi está..',
+      'Tardando mas de lo esperado :(',
+    ];
+
+    return Stream.periodic(const Duration(milliseconds: 1200), (step) {
+      return messages[step];
+    }).take(messages.length);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(children: [
+        const Text('Espere por favor'),
+        const SizedBox(height: 10),
+        const CircularProgressIndicator(strokeWidth: 2),
+        const SizedBox(height: 10),
+
+        StreamBuilder(
+            stream: getLoadingMessages(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const Text('Cargando...');
+              return Text(snapshot.data! );
+            })
+      ]),
+    );
+  }
+}
