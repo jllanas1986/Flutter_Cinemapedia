d6bcc189b53acbf49b1f8f4ffc052b72a8fb1dc1
Jose Llanas <josellanasmota86@gmail.com>
2023-08-17 13:03:41 +0200

    implementing movie detail

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,16 +3,21 @@ import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/', 
   routes: [
-    
     GoRoute(
       path: '/',
       name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
+      routes: [
+        GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) =>
+            MovieScreen(movieId: state.pathParameters['id'] ?? 'no-id')
+        ),
+      ]
     ),
-
-
-
+  
   ]
 );
