f389537435021dffa65b6045e982a8fd0bcf290d
Jose Llanas <josellanasmota86@gmail.com>
2023-08-23 17:40:35 +0200

    Search Movies Providers

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -28,12 +28,17 @@ class CustomAppbar extends ConsumerWidget {
                 IconButton(
                     onPressed: () {
                       final movieRepository = ref.read(movieRepositoryProvider);
+                      final searchQuery = ref.read(searchQueryProvider);
 
                       showSearch<Movie?>(
-                              context: context,
-                              delegate: SearchMovieDelegate(
-                                  searchMovies: movieRepository.searchMovies))
-                          .then((movie) {
+                          query: searchQuery,
+                          context: context,
+                          delegate: SearchMovieDelegate(searchMovies: (query) {
+                            ref
+                                .read(searchQueryProvider.notifier)
+                                .update((state) => query);
+                            return movieRepository.searchMovies(query);
+                          })).then((movie) {
                         if (movie == null) return;
 
                         context.push('/movie/${movie.id}');
