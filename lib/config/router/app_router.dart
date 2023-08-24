e95a79604348fdddaf02cd763633ac10836947c4
Jose Llanas <josellanasmota86@gmail.com>
2023-08-24 09:31:50 +0200

    Preparation of Views

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/views/home_views/favorites_view.dart';
 import 'package:go_router/go_router.dart';
 
 import 'package:cinemapedia/presentation/screens/screens.dart';
@@ -5,11 +6,11 @@ import 'package:cinemapedia/presentation/screens/screens.dart';
 final appRouter = GoRouter(
   initialLocation: '/', 
   routes: [
-    
+
     GoRoute(
       path: '/',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state) => const HomeScreen( childView: FavoritesView(),),
       routes: [
         GoRoute(
           path: 'movie/:id',
