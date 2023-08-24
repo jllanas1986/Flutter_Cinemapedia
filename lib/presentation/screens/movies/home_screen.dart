39b2ae190b04003f58a2a221a8d8c9962eb534b0
Jose Llanas <josellanasmota86@gmail.com>
2023-08-24 12:40:58 +0200

    Operation of the bottom navigation bar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -21,7 +21,7 @@ class HomeScreen extends StatelessWidget {
         index: pageIndex,
         children: viewRoutes,
       ),
-      bottomNavigationBar: CustomBottomNavigation(),
+      bottomNavigationBar: CustomBottomNavigation(currentIndex: pageIndex ),
     );
   }
 }
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -2,36 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  const CustomBottomNavigation({super.key});
 
-  int getCurrentIndex(BuildContext context) {
-    final String location = GoRouterState.of(context).matchedLocation;
+  final int currentIndex;
 
-    switch (location) {
-      case '/':
-        return 0;
+  const CustomBottomNavigation({
+    super.key, 
+    required this.currentIndex
+  });
 
-      case '/categories':
-        return 1;
 
-      case '/favorites':
-        return 2;
+  // int getCurrentIndex(BuildContext context) {
+  //   final String location = GoRouterState.of(context).matchedLocation;
 
-      default:
-        return 0;
-    }
-  }
+  //   switch (location) {
+  //     case '/':
+  //       return 0;
+
+  //     case '/categories':
+  //       return 1;
+
+  //     case '/favorites':
+  //       return 2;
+
+  //     default:
+  //       return 0;
+  //   }
+  // }
 
   void onItemTapped(BuildContext context, int index) {
     switch (index) {
       case 0:
-        context.go('/');
+        context.go('/home/0');
         break;
       case 1:
-        context.go('/');
+        context.go('/home/1');
         break;
       case 2:
-        context.go('/favorites');
+        context.go('/home/2');
         break;
     }
   }
@@ -39,15 +46,16 @@ class CustomBottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-        elevation: 0,
-        currentIndex: getCurrentIndex(context),
-        onTap: (value) => onItemTapped(context, value),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.label_outline), label: 'Categorías'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
-        ]);
+      currentIndex: currentIndex,
+      onTap: (value) => onItemTapped(context, value),
+      elevation: 0,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.label_outline), label: 'Categorías'),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_outline), label: 'Favoritos'),
+        ]
+      );
   }
 }
