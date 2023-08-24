66f73f393a455e87f3b536340d9ec6496a3f3c73
Jose Llanas <josellanasmota86@gmail.com>
2023-08-24 11:06:59 +0200

    Bottom Navigation Bar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,35 +3,41 @@ import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:cinemapedia/presentation/views/views.dart';
 
-final appRouter = GoRouter(initialLocation: '/', routes: [
-  ShellRoute(
+final appRouter = GoRouter(
+  initialLocation: '/',
+  routes: [
+    
+
+    ShellRoute(
       builder: (context, state, child) {
         return HomeScreen(childView: child);
       },
       routes: [
+
         GoRoute(
-            path: '/',
-            builder: (context, state) {
-              return const HomeView();
-            },
-            routes: [
-              GoRoute(
-                  path: 'movie/:id',
-                  name: MovieScreen.name,
-                  builder: (context, state) => MovieScreen(
-                      movieId: state.pathParameters['id'] ?? 'no-id')),
-            ]),
-      ]
-    ),
-        
+          path: '/',
+          builder: (context, state) {
+            return const HomeView();
+          },
+          routes: [
+            GoRoute(
+              path: 'movie/:id',
+              name: MovieScreen.name,
+              builder: (context, state) =>
+                MovieScreen(movieId: state.pathParameters['id'] ?? 'no-id')
+            ),
+          ]
+        ),
+
         GoRoute(
           path: '/favorites',
           builder: (context, state) {
             return const FavoritesView();
           },
         ),
-  ]
-);
+
+      ]
+    ),
 
 
 
@@ -51,3 +57,6 @@ final appRouter = GoRouter(initialLocation: '/', routes: [
   //   ),
   
   // ]
+
+  ]
+);
