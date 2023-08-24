792b55928d4fa19775342b5e472c62daaf0fb2d4
Jose Llanas <josellanasmota86@gmail.com>
2023-08-24 13:45:43 +0200

    Bookmark button and gradients

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -193,6 +193,16 @@ class _CustomSliverAppBar extends StatelessWidget {
       backgroundColor: Colors.black,
       expandedHeight: size.height * 0.7,
       foregroundColor: Colors.white,
+      actions: [
+        IconButton(
+            onPressed: () {
+              //To DO
+            },
+            icon: Icon(Icons.favorite_border)
+            //icon: const Icon( Icons.favorite_rounded )
+            ),
+        //to do
+      ],
       flexibleSpace: FlexibleSpaceBar(
         titlePadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         // title: Text(
@@ -211,6 +221,46 @@ class _CustomSliverAppBar extends StatelessWidget {
                   return FadeIn(child: child);
                 },
               ),
+            ),
+
+            const _CustomGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.0, 0.2],
+              colors: [
+                Colors.black54, 
+                Colors.transparent
+              ]
+            ),
+
+            const _CustomGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.8, 1.0],
+              colors: [
+                Colors.transparent, 
+                Colors.black54
+              ]
+            ),
+
+            const _CustomGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              stops: [0.0, 0.3],
+              colors: [
+                Colors.black87, 
+                Colors.transparent
+              ]
+            ),
+
+            const SizedBox(
+              child: DecoratedBox(
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          stops: [0.0, 0.2],
+                          colors: [Colors.black54, Colors.transparent]))),
             ),
             const SizedBox(
               child: DecoratedBox(
@@ -235,6 +285,35 @@ class _CustomSliverAppBar extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _CustomGradient extends StatelessWidget {
+  final AlignmentGeometry begin;
+  final AlignmentGeometry end;
+  final List<double> stops;
+  final List<Color> colors;
+
+  const _CustomGradient(
+      {this.begin = Alignment.centerLeft,
+      this.end = Alignment.centerRight,
+      required this.stops,
+      required this.colors});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: begin, 
+            end: end, 
+            stops: stops, 
+            colors: colors
+          )
+        )
       ),
     );
   }
