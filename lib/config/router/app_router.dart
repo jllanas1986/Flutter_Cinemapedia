a4e30e9ccec0c3d648c97a800e06885602f073e6
Jose Llanas <josellanasmota86@gmail.com>
2023-08-24 12:24:25 +0200

    Router Configuration

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,43 +1,38 @@
 import 'package:go_router/go_router.dart';
 
 import 'package:cinemapedia/presentation/screens/screens.dart';
-import 'package:cinemapedia/presentation/views/views.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home/0',
   routes: [
     
-
-    ShellRoute(
-      builder: (context, state, child) {
-        return HomeScreen(childView: child);
+    GoRoute(
+      path: '/home/:page',
+      name: HomeScreen.name,
+      builder: (context, state) {
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+        return HomeScreen(pageIndex: pageIndex);
       },
+
       routes: [
-
-        GoRoute(
-          path: '/',
-          builder: (context, state) {
-            return const HomeView();
-          },
-          routes: [
-            GoRoute(
-              path: 'movie/:id',
-              name: MovieScreen.name,
-              builder: (context, state) =>
-                MovieScreen(movieId: state.pathParameters['id'] ?? 'no-id')
-            ),
-          ]
+         GoRoute(
+          path: 'movie/:id',
+          name: MovieScreen.name,
+          builder: (context, state) =>
+            MovieScreen(movieId: state.pathParameters['id'] ?? 'no-id')
         ),
-
-        GoRoute(
-          path: '/favorites',
-          builder: (context, state) {
-            return const FavoritesView();
-          },
-        ),
-
       ]
     ),
+
+
+
+    // GoRoute(
+    //   path: '/',
+    //   redirect: ( _ , __ ) => '/home/0',
+    // ),
+
+  ]
+);
 
 
 
@@ -58,5 +53,4 @@ final appRouter = GoRouter(
   
   // ]
 
-  ]
-);
+
