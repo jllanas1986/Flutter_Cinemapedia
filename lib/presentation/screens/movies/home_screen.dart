d6bea7769eccd890f1563669696d12d59ef52a01
Jose Llanas <josellanasmota86@gmail.com>
2023-08-24 11:18:49 +0200

    Creation views

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,18 +1,17 @@
+import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
-  final Widget childView;
-
-  const HomeScreen({super.key, required this.childView});
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: childView,
-      bottomNavigationBar: const CustomBottomNavigation(),
+    return const Scaffold(
+      body: FavoritesView(),
+      bottomNavigationBar: CustomBottomNavigation(),
     );
   }
 }
