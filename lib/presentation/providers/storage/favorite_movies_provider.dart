bc9c0a8f22d4ce3fb7d0f4c27eb53cf8be203b86
Jose Llanas <josellanasmota86@gmail.com>
2023-08-28 23:05:08 +0200

    Dynamically remove and add elements

--- lib/presentation/providers/storage/favorite_movies_provider.dart
+++ lib/presentation/providers/storage/favorite_movies_provider.dart
@@ -3,7 +3,8 @@ import 'package:cinemapedia/domain/repositories/local_storage_respository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:cinemapedia/presentation/providers/providers.dart';
 
-final favoriteMoviesProvider = StateNotifierProvider<StorageMoviesNotifier, Map<int, Movie>>((ref) {
+final favoriteMoviesProvider =
+    StateNotifierProvider<StorageMoviesNotifier, Map<int, Movie>>((ref) {
   final localStorageRepository = ref.watch(localStorageRepositoryProvider);
   return StorageMoviesNotifier(localStorageRepository: localStorageRepository);
 });
@@ -17,16 +18,15 @@ final favoriteMoviesProvider = StateNotifierProvider<StorageMoviesNotifier, Map<
 */
 
 class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
-  
   int page = 0;
   final LocalStorageRepository localStorageRepository;
 
   StorageMoviesNotifier({required this.localStorageRepository}) : super({});
 
   Future<List<Movie>> loadNextPage() async {
-    final movies = await localStorageRepository.loadMovies(
-      offset: page * 10);
-      page++;
+    final movies =
+        await localStorageRepository.loadMovies(offset: page * 10, limit: 20);
+    page++;
 
     final tempMoviesMap = <int, Movie>{};
     for (final movie in movies) {
@@ -36,5 +36,17 @@ class StorageMoviesNotifier extends StateNotifier<Map<int, Movie>> {
     state = {...state, ...tempMoviesMap};
 
     return movies;
+  }
+
+  Future<void> toggleFavorite(Movie movie) async {
+    await localStorageRepository.toggleFavorite(movie);
+    final bool isMovieInFavorites = state[movie.id] != null;
+
+    if (isMovieInFavorites) {
+      state.remove(movie.id);
+      state = {...state};
+    } else {
+      state = {...state, movie.id: movie};
+    }
   }
 }
